51a97395a645b1d98d99055ecf0fa99709e80a26
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2024-12-12 15:55:49 -0300

    ajustes jest

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  // Lint & Prettify TS and JS files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `yarn run lint --fix ${filenames.map((file) => `--file ${file}`).join(' ')}`
+    `yarn run lint --fix ${filenames
+      .map((file) => `--file ${file}`)
+      .join(' ')}`,
+    `yarn run test --findRelatedTests ${filenames
+      .map((file) => `--file ${file}`)
+      .join(' ')}`
   ]
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,5 @@
 export const Main = () => (
   <main>
-    <h1>uuuuuu</h1>
+    <h1>teste</h1>
   </main>
 )
